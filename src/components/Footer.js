e48b9ddb286b6acbbe83172cb2694f30d676656b
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-02-01 20:44:31 -0600

    created footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Footer() {
+    return (
+
+        <footer>
+            <h4><span id="copyright" class="p-3"></span>2021 Geoffrey Johnson</h4>
+        </footer>
+    );
+}
+
+export default Footer;

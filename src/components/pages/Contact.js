70d3cc7e7dc10bd84297429f67db1fff1a494c42
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-02-03 20:48:14 -0600

    added form to contacts page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,25 +1,51 @@
 import React from "react";
 import { ExternalLink } from "react-external-link";
+import { Form, FormControl, Button } from "react-bootstrap";
 
 function Contact() {
     return (
         <section id="contact" className="row py-4">
             <h2 className="section-header  col-lg-2 col-sm-12 px-3">Contact</h2>
             <div className="section-content col-10">
-                <ExternalLink
-                    className="contact-link"
-                    href="tel:+19202853709">
-                    (920)-285-3709
-                </ExternalLink>
-                <ExternalLink
-                    className="contact-link"
-                    href="mailto:johnson.ge.49@gmail.com">
-                    johnson.ge.49@gmail.com
-                </ExternalLink>
+                <div>
+
+                    <h5>Phone:</h5>
+                    <Button className="btn-info">
+                        <ExternalLink
+                            className="text-black"
+                            href="tel:+19202853709">
+                            (920)-285-3709
+                        </ExternalLink>
+                    </Button>
+                </div>
+                <div>
+
+                    <h5>Email:</h5>
+                    <Button className="btn-info">
+                        <ExternalLink
+                            className="text-black"
+                            href="mailto:johnson.ge.49@gmail.com">
+                            johnson.ge.49@gmail.com
+                        </ExternalLink>
+                    </Button>
+                </div>
             </div>
 
             <form>
+                <Form.Group className="mb-3" controlId="contact-form">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="input" placeholder="Name" />
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <FormControl as="textarea" aria-label="With textarea" placeholder="Type your message here" />
+                    <Form.Text className="text-muted">
+                        **This is a test form with no back-end configured.**
+                    </Form.Text>
+                    <br></br>
+                    <Button className="btn-info"
+                        type="submit">Submit form</Button>
 
+                </Form.Group>
             </form>
         </section>
     );

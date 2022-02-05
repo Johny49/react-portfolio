892ba0e69f241dafa97fc931cfaa935af4e72f64
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-02-04 22:40:25 -0600

    styling and formatting

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,27 +1,40 @@
 import React from "react";
-import { ExternalLink } from "react-external-link";
-import { ProjectImg0 } from "../../images/index";
+import Spacer from "../Spacer";
+import { Button } from "react-bootstrap";
+
+const styles = {
+    cardTitle: {
+        color: '#022b3a',
+        padding: '0 20px',
+    }
+};
 
 function Project({ name, ghLink, deployedLink, type, imgSrc, alt }) {
-
     return (
-        <div className="container">
+        <div className="container col-10">
             <div className="card">
                 <img
                     className="card-imgSrc-top"
                     src={imgSrc}
                     alt={"project.alt"}
                 />
-                <div className="card-body">
-                    <h5 className="card-title">{name}</h5>
-                    <a href={ghLink} className="btn btn-info">
-                        View Repo on Github
-                    </a>
-                    <a href={deployedLink} className="btn btn-info">
-                        View Deployed
-                    </a>
+                <div className="card-body text-center">
+                    <h5 className="card-title" style={styles.cardTitle}>{name}</h5>
+                    <div>
+                        <Button className="btn btn-info m-4">
+                            <a href={ghLink} target="_blank" rel="noreferrer">
+                                View Repo on Github
+                            </a>
+                        </Button>
+                        <Button className="btn btn-info m-4">
+                            <a href={deployedLink} target="_blank" rel="noreferrer" >
+                                View Deployed
+                            </a>
+                        </Button>
+                    </div>
                 </div>
             </div>
+            <Spacer />
         </div>
     );
 }
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,27 +1,31 @@
-import React, { useState } from "react";
-import { ExternalLink } from "react-external-link";
+import React from "react";
+import Spacer from "../Spacer";
 import { ProjectImg0, ProjectImg1, ProjectImg2, ProjectImg3, ProjectImg4, ProjectImg5 } from '../../images/index';
 import Project from "./Project";
 
 const Projects = () => {
     const projects = [
-        { name: "Weather Dashboard", ghLink: "https://github.com/Johny49/weather-dashboard/", deployedLink: "https://johny49.github.io/weather-dashboard/", type: "site", imgSrc: ProjectImg0, alt: "screenshot of weather dashboard" },
-        { name: "Test Your Knowledge", ghLink: "https://github.com/Johny49/test-your-knowledge/", deployedLink: "https://johny49.github.io/test-your-knowledge/", type: "site", imgSrc: ProjectImg1, alt: "screenshot of test your knowledge webpage" },
-        { name: "Password Tracker", ghLink: "https://github.com/Johny49/password-generator/", deployedLink: "https://johny49.github.io/password-generator/", type: "site", imgSrc: ProjectImg2, alt: "screenshot of password generator webpage" },
+        { name: "Weather Dashboard", ghLink: "https://github.com/Johny49/weather-dashboard/", deployedLink: "https://johny49.github.io/weather-dashboard/", type: "site", imgSrc: ProjectImg0, alt: "screenshot of weather dashboard", key: 0 },
+        { name: "Test Your Knowledge", ghLink: "https://github.com/Johny49/test-your-knowledge/", deployedLink: "https://johny49.github.io/test-your-knowledge/", type: "site", imgSrc: ProjectImg1, alt: "screenshot of test your knowledge webpage", key: 1 },
+        { name: "Password Generator", ghLink: "https://github.com/Johny49/password-generator/", deployedLink: "https://johny49.github.io/password-generator/", type: "site", imgSrc: ProjectImg2, alt: "screenshot of password generator webpage", key: 2 },
         {
             name: "Employee Tracker CMS", ghLink: "https://github.com/Johny49/employee-tracker-cms", deployedLink: "https://drive.google.com/drive/folders/17AvkjUoSTC1Nji76Mpb2CnqLLxsnVDh5", type: "video", imgSrc:
-                ProjectImg3, alt: "screenshot of plan it work day scheduler"
+                ProjectImg3, alt: "screenshot of plan it work day scheduler", key: 3
         },
-        { name: "E-Commerce Backend", ghLink: "https://github.com/Johny49/e-commerce-back-end", deployedLink: "https://drive.google.com/drive/folders/1BQRRPhXHZzI-hGZ5gswIW9H_-gHgJeNy", type: "video", imgSrc: ProjectImg4, alt: "screenshot of Pokedex website" },
-        { name: "Tapper", ghLink: "https://github.com/Johny49/tapper", deployedLink: "#", type: "none", imgSrc: ProjectImg5, alt: "screenshot of Tapper app on an iPad" }]
+        { name: "E-Commerce Backend", ghLink: "https://github.com/Johny49/e-commerce-back-end", deployedLink: "https://drive.google.com/drive/folders/1BQRRPhXHZzI-hGZ5gswIW9H_-gHgJeNy", type: "video", imgSrc: ProjectImg4, alt: "screenshot of Pokedex website", key: 4 },
+        { name: "Tapper", ghLink: "https://github.com/Johny49/tapper", deployedLink: "#", type: "none", imgSrc: ProjectImg5, alt: "screenshot of Tapper app on an iPad", key: 5 }]
 
     //key = project.name
     return (
-        <div>
-            {projects.map(project => (
-                < Project {...project} />
-            ))}
-        </div>
+        <section>
+            <h2 className="section-header  col-12 p-3">Projects</h2>
+            <Spacer />
+            <div>
+                {projects.map(project => (
+                    < Project {...project} />
+                ))}
+            </div>
+        </section>
     );
 }
 

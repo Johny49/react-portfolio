bb38e79b30fa9193c37a5bde2139fa96b6dee805
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-02-05 10:47:10 -0600

    cleanup and a11y optimisation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Spacer from "./Spacer";
 import { ExternalLink } from 'react-external-link';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from "react-bootstrap";
@@ -7,34 +8,26 @@ function Footer() {
     return (
 
         <footer className="text-center">
-            <br></br>
+            <Spacer />
             <section>
-                {/* <ul className="list-group list-group-horizontal justify-content-center"> */}
-                {/* <li className="list-group-item"> */}
                 <Button className="btn-info mx-1">
-                    <ExternalLink className="text-light" href="https://github.com/Johny49" target="_blank" rel="noopener noreferrer">
+                    <ExternalLink href="https://github.com/Johny49" target="_blank" rel="noopener noreferrer">
                         <span>Github</span>
                     </ExternalLink>
                 </Button>
-                {/* </li> */}
-                {/* <li className="list-group-item"> */}
                 <Button className="btn-info mx-1">
-                    <ExternalLink className="text-light" href="https://www.linkedin.com/in/johnsonge49/" target="_blank" rel="noopener noreferrer">
+                    <ExternalLink href="https://www.linkedin.com/in/johnsonge49/" target="_blank" rel="noopener noreferrer">
                         <span>LinkedIn</span>
                     </ExternalLink>
                 </Button>
-                {/* </li> */}
-                {/* <li className="list-group-item"> */}
                 <Button className="btn-info mx-1">
-                    <ExternalLink className="text-light" href="https://stackoverflow.com/users/17213151/" target="_blank" rel="noopener noreferrer">
+                    <ExternalLink href="https://stackoverflow.com/users/17213151/" target="_blank" rel="noopener noreferrer">
                         <span>Stack Overflow</span>
                     </ExternalLink>
                 </Button>
-                {/* </li> */}
-                {/* </ul> */}
             </section>
             <hr></hr>
-            <h4>{`\u00a9`} 2021 Geoffrey Johnson</h4>
+            <h6>{`\u00a9`} 2021 Geoffrey Johnson</h6>
         </footer>
     );
 }
